895b3abbb4b322e5510a2db4c2727a7ea0e92837
rikarti <tep161a@gmail.com>
2025-11-25 18:40:14 +0300

    cursor well done

---
+++ app/src/main/java/ru/hopes/workouttimer/presentation/utils/VibrationManager.kt
@@ -0,0 +1,38 @@
+package ru.hopes.workouttimer.presentation.utils
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class VibrationManager @Inject constructor(
+    @ApplicationContext context: Context
+) {
+
+    private val vibrator: Vibrator? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val manager = context.getSystemService(VibratorManager::class.java)
+        manager?.defaultVibrator
+    } else {
+        @Suppress("DEPRECATION")
+        context.getSystemService(Context.VIBRATOR_SERVICE) as? Vibrator
+    }
+
+    fun vibrate(durationMillis: Long = 700L) {
+        val vib = vibrator ?: return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vib.vibrate(
+                VibrationEffect.createOneShot(
+                    durationMillis,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            vib.vibrate(durationMillis)
+        }
+    }
+}
+
